d1c6eedc8797980a19a838b57fa1c202652fbc28
jamie-codez <cruiseomondi90@gmail.com>
2024-03-08 02:57:09 +0300

    frontend:sidebar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import {Route, Routes} from "react-router-dom";
+import {SettingsPage} from "./pages/SettingsPage.jsx";
+import {AboutPage} from "./pages/AboutPage.jsx";
+import {LikesPage} from "./pages/LikesPage.jsx";
+import {ExplorePage} from "./pages/ExplorePage.jsx";
+import {ProfilePage} from "./pages/ProfilePage.jsx";
+import {RegisterPage} from "./pages/RegisterPage.jsx";
+import {LoginPage} from "./pages/LoginPage.jsx";
+import {HomePage} from "./pages/HomePage.jsx";
+import {Sidebar} from "./components/Sidebar.jsx";
+
+function App() {
+
+    return (
+        <div className={'flex'}>
+            <Sidebar/>
+            <div className={'max-w-5x1 my-5 text-white mx-auto transition-all duration-300 flex-1'}>
+                <Routes>
+                    <Route path={'/'} element={<HomePage/>}/>
+                    <Route path={'/login'} element={<LoginPage/>}/>
+                    <Route path={'/register'} element={<RegisterPage/>}/>
+                    <Route path={'/profile'} element={<ProfilePage/>}/>
+                    <Route path={'/explore'} element={<ExplorePage/>}/>
+                    <Route path={'/likes'} element={<LikesPage/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/settings'} element={<SettingsPage/>}/>
+                </Routes>
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ frontend/src/pages/LikesPage.jsx
@@ -0,0 +1,5 @@
+export const LikesPage = () => {
+    return (
+        <>Likes Page</>
+    )
+}
