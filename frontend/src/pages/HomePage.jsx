d184c633a1289b2aa5fbf9619071a17770aba455
jamie-codez <cruiseomondi90@gmail.com>
2024-03-08 03:33:12 +0300

    frontend:search component and home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
 export const HomePage = () => {
     return (
-        <>Home Page</>
-    )
-}
+        <div className='m-4'>
+            <Search />
+            <SortRepos />
+            <div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+                <ProfileInfo />
+                <Repos />
+                <Spinner />
+            </div>
+        </div>
+    );
+};
